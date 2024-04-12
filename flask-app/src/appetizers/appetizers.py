221c22cc935896792a22948bad87ed2b99f68cac
jpanct <jlongct@gmail.com>
2024-04-12 14:53:05 -0400

    adding files

--- flask-app/src/appetizers/appetizers.py
+++ flask-app/src/appetizers/appetizers.py
@@ -1,1 +1,11 @@
--- jamie is doing 
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+import json
+from flaskr.db import get_db
+from src import db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+appetizers = Blueprint('appetizers', __name__)
